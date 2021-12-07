f18868d4345e1a7b1b28cbc103cdd8fac33762d1
Aimj <user@DESKTOP-17V2CUE>
2021-12-07 15:14:08 +0200

    second commit

--- data visualization.R
+++ data visualization.R
@@ -29,3 +29,62 @@ ggplot(data = mpg)+
 ggplot(data = mpg)+
   geom_point(mapping = aes(x=displ,y=hwy,color=displ<5))+
   facet_wrap(~class,nrow = 2)
+# page 16
+#Geometric Objects
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x=displ,y=hwy),color='blue')
+
+ggplot(data = mpg)+
+  geom_smooth(mapping = aes(x=displ,y=hwy))
+
+ggplot(data = mpg)+
+  geom_smooth(mapping = aes(x=displ,y=hwy),color='orange')
+
+ggplot(data = mpg)+
+  geom_smooth(mapping = aes(x=displ,y=hwy,linetype=drv,color=drv))+
+  geom_point(mapping = aes(x=displ,y=hwy,color=drv))
+
+ggplot(data = mpg)+
+  geom_smooth(mapping = aes(x=displ,y=hwy,group=drv),color='orange')
+
+ggplot(data = mpg,mapping = aes(x=displ,y=hwy))+
+  geom_smooth(mapping = aes(linetype=drv,color=drv))+
+  geom_point(mapping = aes(color=drv))
+
+ggplot(data = mpg,mapping = aes(x=displ,y=hwy))+
+  geom_smooth(data = filter(mpg,class=='subcompact'))+
+  geom_point(mapping = aes(color=class))
+
+ggplot(data = mpg,mapping = aes(x=displ,y=hwy))+
+  geom_smooth(data = filter(mpg,class=='subcompact'),se=FALSE)+
+  geom_point(mapping = aes(color=class))
+
+ggplot(data = mpg,mapping = aes(x=displ,y=hwy,color=drv))+
+  geom_point()+
+  geom_smooth(se=FALSE)
+
+# Statistical transformations
+
+head(diamonds,5)
+
+#Bar chart
+ggplot(data = diamonds,mapping = aes(x=cut))+
+  geom_bar()
+
+ggplot(data = diamonds,mapping = aes(x=cut))+
+  stat_count()
+
+trial <- tribble(
+  ~a,    ~b,
+  'bar1',25,
+  'bar2',35,
+  'bar3',75
+  )
+ggplot(data = trial,mapping = aes(x=a,y=b))+
+  geom_bar(stat = 'identity')
+
+ggplot(data = diamonds,mapping = aes(x=cut))+
+  geom_bar(mapping = aes(y=..prop..,group=1))
+
+ggplot(data = diamonds,mapping = aes(x=cut,y=depth))+
+  stat_summary(fun.max = max,fun.min = min,fun = median)

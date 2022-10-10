3dd88cafa66deaf667f4137da6ffb61df166d13f
AimjGuytidy <aimjguy@gmail.com>
2022-10-10 10:12:08 +0200

    "analyzing the quality of life indicator"

---
+++ quality_of_life_analysis.R
@@ -0,0 +1,69 @@
+library(dplyr)
+library(tidyr)
+library(readr)
+library(openxlsx)
+library(haven)
+library(foreign)
+library(labelled)
+library(rio)
+library(psych)
+library(ggplot2)
+library(stringr)
+library(forcats)
+library(data.table)
+Light_grey <- c("#F2F2F2") #Light grey for the background
+Blue <- c("#097ABC") #Blue
+Light_blue <- c("#9DC3E5") #Light blue
+Dark_blue <- c("#1b2f55") #Dark blue
+Green <- c("#8ccc98") #Green
+Dark_grey <- c("#7F7F7F") #Dark grey
+Dark_green <- c("#49711E") #Dark green
+
+mcf_data <- read_sav("data/mcf_data_new_reporting_tool.sav")
+mcf_data_l5_t <- mcf_data
+
+keyword_label<-c("D14",	"D16",	"D18",	"D20",	"D22",	"D24",	"D26",	"D28",	"D30",	"D32",	"D34",	"D36")
+variables_for_l531_a <- mcf_data_l5_t%>%look_for(keyword_label)
+variables_for_l531_a <-variables_for_l531_a[,"variable"]
+var_df <- as.data.frame(variables_for_l531_a)
+
+for (i in 1:length(keyword_label) ){
+  mcf_data_l5_t[,var_df[i,]][mcf_data_l5_t[,var_df[i,]]==1]<-0
+  mcf_data_l5_t[,var_df[i,]][mcf_data_l5_t[,var_df[i,]]==2]<-1
+  mcf_data_l5_t[,var_df[i,]][mcf_data_l5_t[,var_df[i,]]==3]<-2
+  mcf_data_l5_t[,var_df[i,]][mcf_data_l5_t[,var_df[i,]]==4]<-3
+  mcf_data_l5_t[,var_df[i,]][mcf_data_l5_t[,var_df[i,]]==5]<-4
+}
+
+# change the values from 1-3 to 0-2 
+keyword_label_b<-c("D15",	"D17",	"D19",	"D21",	"D23",	"D25",	"D27",	"D29",	"D31",	"D33",	"D35",	"D37")
+variables_for_l531_b <- mcf_data_l5_t%>%look_for(keyword_label_b)
+variables_for_l531_b <-variables_for_l531_b[,"variable"]
+var_df_b <- as.data.frame(variables_for_l531_b)
+
+for (i in 1:length(keyword_label_b) ){
+  mcf_data_l5_t[,var_df_b[i,]][mcf_data_l5_t[,var_df_b[i,]]==1]<-0
+  mcf_data_l5_t[,var_df_b[i,]][mcf_data_l5_t[,var_df_b[i,]]==2]<-1
+  mcf_data_l5_t[,var_df_b[i,]][mcf_data_l5_t[,var_df_b[i,]]==3]<-2
+}
+
+# step 1: summing values from variables covering subquestion a and indicator l5.3.1
+#var_df_filter <- grep("_access$", var_df$variable,value=TRUE, ignore.case =T)
+mcf_data_l5_t<-mcf_data_l5_t%>%
+  mutate(sum_quality_life=rowSums(select(.,grep("_access$", var_df$variable,value=TRUE, ignore.case =T)
+  ),na.rm = TRUE))
+
+# step 2: averaging services improvement (subquestion b related)
+
+mcf_data_l5_t<-mcf_data_l5_t%>%
+  mutate(avg_improv_quality_life=rowMeans(select(.,var_df_b$variable),na.rm = TRUE))
+
+#step 3: computing the product of step 1 and step 2
+
+mcf_data_l5_t<-mcf_data_l5_t%>%
+  mutate(prod_quality_life=avg_improv_quality_life*sum_quality_life)
+
+#step 4: adjusting the index to 100 from step 3
+
+mcf_data_l5_t<-mcf_data_l5_t%>%
+  mutate(perc_quality_life=(prod_quality_life*100)/96)

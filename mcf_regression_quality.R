6ab72a464a5e4155b1f964523c04ab76c34e4495
AimjGuytidy <aimjguy@gmail.com>
2022-10-11 11:56:25 +0200

    commit

---
+++ mcf_regression_quality.R
@@ -0,0 +1,50 @@
+library(glmnet)
+library(dplyr)
+library(haven)
+library(psych)
+library(tibble)
+
+mcf_data_df_shock<-read_dta("data/mcf_data_qual_regression.dta")
+#regression with df against quality of life
+
+X <- as.data.frame(mcf_data_df_shock$DFWIA1)
+X <- X%>%
+  mutate(dignified=ifelse(mcf_data_df_shock$DFWIA1==1,1,0),
+         undignified=ifelse(mcf_data_df_shock$DFWIA1==0,1,0))
+x <-data.matrix(X[,c("dignified","undignified")])
+y <- mcf_data_df_shock$perc_quality_life
+
+
+cv_model <- cv.glmnet(x,y,alpha = 1)
+best_lambda <- cv_model$lambda.min
+plot(cv_model)
+best_model <- glmnet(x,y,alpha=1,lambda = best_lambda)
+sink("coefficients.txt")
+coef(best_model)
+sink()
+
+#Regression with df , adaptive and absorptive against quality of life index
+
+
+(adapt_qual <- ggplot(data = mcf_data_df_shock,aes(perc_quality_life,
+                                    adaptative_index))+
+  geom_point()+
+  geom_smooth())
+
+(absorp_qual <- ggplot(data = mcf_data_df_shock,aes(perc_quality_life,
+                                                   absorptive_index))+
+  geom_point()+
+  geom_smooth())
+
+
+x <-data.matrix(mcf_data_df_shock[,c("adaptative_index","absorptive_index")])
+y <- mcf_data_df_shock$perc_quality_life
+
+
+cv_model <- cv.glmnet(x,y,alpha = 1)
+best_lambda <- cv_model$lambda.min
+plot(cv_model)
+best_model <- glmnet(x,y,alpha=1,lambda = best_lambda)
+sink("coefficients.txt")
+coef(best_model)
+sink()

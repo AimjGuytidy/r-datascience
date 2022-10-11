493120e8b1c02e39051437bd40b4297ac7e08d52
AimjGuytidy <aimjguy@gmail.com>
2022-10-11 12:15:31 +0200

    commit

--- mcf_regression_quality.R
+++ mcf_regression_quality.R
@@ -36,15 +36,52 @@ sink()
   geom_point()+
   geom_smooth())
 
+data_use <- mcf_data_df_shock[,c("uniqueid",
+                                "adaptative_index","absorptive_index",
+                                "perc_quality_life")]
+data_use <- drop_na(data_use)
 
-x <-data.matrix(mcf_data_df_shock[,c("adaptative_index","absorptive_index")])
-y <- mcf_data_df_shock$perc_quality_life
+x <-data.matrix(data_use[,c("adaptative_index","absorptive_index")])
+y <- data_use$perc_quality_life
 
 
 cv_model <- cv.glmnet(x,y,alpha = 1)
 best_lambda <- cv_model$lambda.min
 plot(cv_model)
 best_model <- glmnet(x,y,alpha=1,lambda = best_lambda)
-sink("coefficients.txt")
+sink("coefficients2.txt")
+coef(best_model)
+sink()
+
+
+# Regression with 
+
+#Regression with df against adaptive and absorptive
+
+
+(adapt_qual <- ggplot(data = mcf_data_df_shock,aes(perc_quality_life,
+                                                   adaptative_index))+
+    geom_point()+
+    geom_smooth())
+
+(absorp_qual <- ggplot(data = mcf_data_df_shock,aes(perc_quality_life,
+                                                    absorptive_index))+
+    geom_point()+
+    geom_smooth())
+
+data_use <- mcf_data_df_shock[,c("uniqueid",
+                                 "adaptative_index","absorptive_index",
+                                 "perc_quality_life")]
+data_use <- drop_na(data_use)
+
+x <-data.matrix(data_use[,c("adaptative_index")])
+y <- data_use$perc_quality_life
+
+
+cv_model <- cv.glmnet(x,y,alpha = 1)
+best_lambda <- cv_model$lambda.min
+plot(cv_model)
+best_model <- glmnet(x,y,alpha=1,lambda = best_lambda)
+sink("coefficients2.txt")
 coef(best_model)
 sink()

f16e91ddbb5a186956f3c859ee2cdd17549bddb4
AimjGuytidy <aimjguy@gmail.com>
2023-01-30 15:05:09 +0200

    commit

--- data visualization.R
+++ data visualization.R
@@ -1281,7 +1281,20 @@ diamonds%>%
 #install.packages("heatmaply")
 require(heatmaply)
 #page 103
+ggplot(data = diamonds)+
+  geom_point(
+    mapping = aes(x = carat, y = price),
+    alpha = 1/100
+  )
+ggplot(data = diamonds)+
+  geom_bin2d(mapping = aes(x = carat, y = price))
 
+ggplot(data = diamonds)+
+  geom_hex(mapping = aes(x = carat, y = price))
+
+ggplot(data = diamonds, mapping = aes(x = carat, y = price))+
+  geom_boxplot(mapping = aes(group = cut_width(carat, 0.2)))+
+  coord_cartesian(xlim = c(0,3))
 ################
 
 

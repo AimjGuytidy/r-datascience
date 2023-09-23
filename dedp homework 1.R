fe8dfd363eb1370bfabd433f1e109fdb735914a9
AimjGuytidy <aimjguy@gmail.com>
2023-09-23 23:51:45 +0200

    homework

--- dedp homework 1.R
+++ dedp homework 1.R
@@ -3,3 +3,8 @@ library(tidyverse)
 papers <- as_tibble(read_csv("data/CitesforSara.csv"))
 papers_select <- select(papers,journal,year,cites,title,au1)
 nrow(filter(papers_select,cites>=100))
+
+view(count(papers_select,journal,cites))
+papers%>%
+  group_by(journal)%>%
+  summarise(cites_total=sum(cites,na.rm = TRUE))

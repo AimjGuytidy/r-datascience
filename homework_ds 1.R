2507fe717f3760a4efaed62f9d07ea9a88dcddca
AimjGuytidy <aimjguy@gmail.com>
2023-10-03 04:25:59 +0200

    ds

--- homework_ds 1.R
+++ homework_ds 1.R
@@ -10,3 +10,7 @@ getwd() # make sure I am in the right directory
 gender_data <- as_tibble(read_csv("data/Gender_StatsData.csv"))
 
 view(head(gender_data,3))
+
+# creating a tibble with only adolescent fertility rate indicator
+teenager_fr <- filter(gender_data, Indicator.Code=="SP.ADO.TFRT") # we can use subset too!
+

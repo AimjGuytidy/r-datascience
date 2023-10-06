7d964464c42102b6d8866fb87582b33e9ec7ac06
AimjGuytidy <aimjguy@gmail.com>
2023-10-06 09:06:40 +0200

    Homework correction

--- HW_3_Part_2.R
+++ HW_3_Part_2.R
@@ -4,7 +4,7 @@ rm(list=ls())
 library("utils")
 #install.packages('plot3D')
 library(plot3D)
-setwd()
+#setwd()
 
 #Creating the vector x and y
 M <- mesh(seq(0,1,length=100), seq(0,1,length=100))
@@ -13,4 +13,18 @@ y<-M$y
 z<-6/5*(M$x+M$y^2)
 
 #Plotting this pdf
-persp3D(x, y, z, xlab='X variable', ylab= 'Y variable', xlim = c(0,1), main= "Plotting joint pdf")
+persp3D(x, y, z, xlab='X variable', ylab= 'Y variable', xlim = c(0,1), 
+        main= "Plotting joint pdf")
+set.seed(42)
+x <- seq(0,1,length = 1000)
+y <- seq(0,1,length = 1000)
+X <- function(x){(6/5) *(x**2/2 + (x*1/3))}
+Y <- function(y){(6/5) *(y**3/3 + (y*1/2))}
+ecdfx <- ecdf(X)
+ecdfy <- ecdf(Y)
+plot(ecdf(X))
+plot(ecdf(Y))
+hold
+ggplot(data = data.frame(x), aes(x)) +
+  stat_function(fun = X,color="blue") + 
+  stat_function(fun = Y, color = "red")

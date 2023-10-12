0550cea6d8072040059f329472ed6c43313b1c70
AimjGuytidy <aimjguy@gmail.com>
2023-10-12 05:13:15 +0200

    Hypergeometric distribution further exploration

--- homework_ds 3.R
+++ homework_ds 3.R
@@ -2,6 +2,8 @@
 library(ggplot2)
 library(tidyverse)
 require(cowplot)
+
+# Scenario  1 where we don't accept defects
 N <- 100
 m <- 6
 n <- N - m
@@ -12,4 +14,13 @@ while (prob>.1){
   
   k <- k + 1
   prob <- dhyper(x,m,n,k)
+}
+
+# Scenario 2 where we accept at most 1 defect
+k <- 0
+x <- 1
+prob <- phyper(x,m,n,k)
+while (prob>.1) {
+  k <- k + 1
+  prob <- phyper(x,m,n,k)
 }

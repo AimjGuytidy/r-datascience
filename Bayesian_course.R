ae3f9e5718328b942725efad1c7e07b5b4a30e57
AimjGuytidy <aimjguy@gmail.com>
2023-11-05 13:20:08 +0200

    Calculating posterior using grid approximation

--- Bayesian_course.R
+++ Bayesian_course.R
@@ -15,3 +15,23 @@ priors <- ways/sum(ways)
 
 # binomial distribution
 dbinom(6, size = 9, prob = 0.5) #prob to get 6 waters if prob of water is 0.5
+
+# Grid approximation method ####
+###############################
+
+# define grid
+p_grid <- seq(from = 0, to = 1, length.out = 20)
+
+# define priors
+prior <-rep(1, 20)
+
+# compute likelihood at each value
+likelihood <- dbinom(6, size = 9, prob = p_grid)
+
+# compute product of likelihood and prior
+
+unstd.posterior <- likelihood * prior
+
+# standardize the posterior
+
+posterior <- unstd.posterior/sum(unstd.posterior) 

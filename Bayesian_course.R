af8b097144a30d5e750fbfee168a118fc99f0a72
AimjGuytidy <aimjguy@gmail.com>
2023-11-05 10:39:58 +0200

    count to probability

--- Bayesian_course.R
+++ Bayesian_course.R
@@ -1,3 +1,9 @@
+rm(list = ls())
 #install.packages(c("coda","mvtnorm","devtools","dagitty"))
 library(devtools)
 devtools::install_github("rmcelreath/rethinking")
+
+# from counts to probability
+
+ways <- c( 0, 3, 8, 9, 0 )
+priors <- ways/sum(ways)

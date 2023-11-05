990a2414e671068df0356b810b43590cbae34a78
AimjGuytidy <aimjguy@gmail.com>
2023-11-05 13:01:22 +0200

    Installed Rstan

--- Bayesian_course.R
+++ Bayesian_course.R
@@ -2,7 +2,12 @@ rm(list = ls())
 #install.packages(c("coda","mvtnorm","devtools","dagitty"))
 library(devtools)
 devtools::install_github("rmcelreath/rethinking")
+# installing stan
+# run the next line if you already have rstan installed
+# remove.packages(c("StanHeaders", "rstan"))
 
+install.packages("rstan",
+                 repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 # from counts to probability
 
 ways <- c( 0, 3, 8, 9, 0 )

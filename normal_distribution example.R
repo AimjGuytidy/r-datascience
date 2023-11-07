c67fd0f33579b42954981a7ec8f22c63c16f5419
AimjGuytidy <aimjguy@gmail.com>
2023-11-07 09:11:36 +0200

    log transformation of gaussian

--- normal_distribution example.R
+++ normal_distribution example.R
@@ -4,3 +4,6 @@ mu <- mean(x)
 sigma <- sd(x)
 y <- (1/(sigma*sqrt(2*pi))) * exp(-0.5*(((x-mu)/sigma)^2))
 plot(x,y)
+
+# the log of gaussian forms a parabola which is a quadratic function
+plot(x,log(y))

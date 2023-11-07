75cfca8e29d0ccd8219175c6ee6f781ba83e5e72
AimjGuytidy <aimjguy@gmail.com>
2023-11-07 15:31:03 +0200

    Teachers' data analysis

--- tmis_analysis.R
+++ tmis_analysis.R
@@ -363,3 +363,137 @@ df_age_brackets_teachers_long <- df_age_brackets_teachers |>
 write.xlsx(df_age_brackets_teachers_long,
            "01_tables//retirement_data.xlsx",
            asTable = T)
+
+# Data analysis by considering teachers only ####
+#################################################
+
+# Retirement by qualification and gender and teachers
+ret_qual_teacher <- df_age_brackets_teachers_long |>
+  filter(Age >= 65,Year==2023) |>
+  select(gender,qualificationLevel) |>
+  group_by(qualificationLevel,gender) |>
+  summarise(Total = n())
+write.xlsx(ret_qual_teacher,"01_tables//retirement_qualification_teachers.xlsx",asTable = T)
+
+ret_qual_teacher[nrow(ret_qual_teacher)+1,"gender"] <- "Female"
+ret_qual_teacher[nrow(ret_qual_teacher),"Total"] <- 0
+ret_qual_teacher[nrow(ret_qual_teacher),"qualificationLevel"] <- "A1"
+
+resolution(ret_qual_teacher$Total)
+
+ggplot(data = ret_qual_teacher,aes(x = qualificationLevel,y=Total)) +
+  geom_bar(aes(x = qualificationLevel,y=Total,fill = gender), 
+           stat="identity", position = "dodge")+
+  geom_text(aes(label=Total,group = gender),
+            position = position_dodge(1), size = 3, hjust = .6, 
+            vjust = -.3,fontface="bold",color="#232D3F")+
+  ggtitle("Retirement Age by Qualification")+
+  scale_fill_manual(name = NULL, values = c("Female" = "#FF9130",
+                                            "Male" = "#3876BF"))+
+  theme(plot.title = element_text(hjust = .5),
+        axis.ticks = element_blank())+
+  theme(
+    plot.background = element_rect(fill = c("#F2F2F2")),
+    panel.background = element_rect(fill = c("#F2F2F2")),
+    panel.grid = element_blank(),
+    #remove x axis ticks
+    #axis.text.x = element_blank(),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    #remove x axis labels
+    axis.ticks.x = element_blank(),  #remove x axis ticks
+    axis.text.y = element_blank(),
+    legend.box = "horizontal",
+    legend.position = "bottom"
+  ) 
+
+
+retirement_teacher <- dplyr::count(dplyr::filter(df_age_brackets_teachers_long,
+                                                 Age >= 65,role == "Teacher"),
+                                   Year,teachingCategoryName)|>
+  ungroup()
+
+retirement_bind_teacher <- df_age_brackets_teachers_long |>
+  dplyr::filter(Age >= 65,role == "Teacher") |>
+  group_by(Year) |>
+  mutate(Total = n())|>
+  select(Year,Total) |>
+  rename(n=Total) |>
+  group_by(Year) |>
+  mutate(count = n(),dup = ifelse(count==1,1,row_number())) |>
+  filter(dup == 1) |>
+  mutate(dup = NULL,count = NULL)
+
+ret_binded_teacher <-
+  rbind(
+    retirement_teacher,
+    mutate(retirement_bind_teacher,
+           teachingCategoryName = NA_character_)
+  ) |>
+  mutate(
+    teachingCategoryName = if_else(is.na(teachingCategoryName), "Total",
+                                   teachingCategoryName),
+    Total = NULL
+  ) |>
+  rename(Total = n) |>
+  arrange(Year)
+
+x <- 2023:2030
+y <- ret_binded_teacher[ret_binded_teacher$teachingCategoryName=="Total","Total"]$Total 
+mod <- lm(y~x)
+mod.coef <- coef(mod)
+
+res <- resolution(ret_binded_teacher$Total)
+Year <- 2023:2030
+
+ggplot(data = ret_binded_teacher, aes(x = Year, y = Total)) +
+  geom_bar(
+    aes(x = Year, y = Total, fill = teachingCategoryName),
+    stat = "identity",
+    position = "dodge"
+  ) +
+  geom_text(
+    aes(label = Total, group = teachingCategoryName),
+    position = position_dodge(0.9),
+    size = 3,
+    hjust = .6,
+    vjust = -.3,
+    fontface = "bold",
+    color = "#232D3F"
+  ) +
+  ggtitle("Projected Retirement 2023-2030") +
+  scale_fill_manual(
+    name = NULL,
+    values = c(
+      "PRE_PRIMARY" = "#5272F2",
+      "PRIMARY" = "#B4B4B3",
+      "SECONDARY" = "#0174BE",
+      "Total" = "#4F709C"
+    )
+  ) +
+  theme(plot.title = element_text(hjust = .5),
+        axis.ticks = element_blank()) +
+  theme(
+    plot.background = element_rect(fill = c("#F2F2F2")),
+    panel.background = element_rect(fill = c("#F2F2F2")),
+    panel.grid = element_blank(),
+    #remove x axis ticks
+    #axis.text.x = element_blank(),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    #remove x axis labels
+    axis.ticks.x = element_blank(),
+    #remove x axis ticks
+    axis.text.y = element_blank(),
+    legend.box = "horizontal",
+    legend.position = "bottom"
+  ) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 2500))  +
+  scale_x_continuous(name = NULL,
+                     labels = as.character(Year),
+                     breaks = Year) +
+  geom_abline(slope = mod.coef[["x"]],
+              intercept = mod.coef[["(Intercept)"]],
+              col = "#4F709C") 
+
+write.xlsx(ret_binded_teacher,"01_tables//retirement_trend_teacher.xlsx",asTable = T)

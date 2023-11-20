e284f82908e849079c152526190286b10951d87c
AimjGuytidy <aimjguy@gmail.com>
2023-11-20 10:46:42 +0200

    Geocentric models

--- Bayesian_course.R
+++ Bayesian_course.R
@@ -425,15 +425,9 @@ plot(density(replicate(1000,sum(runif(4,-1,1)))))
 temp <- replicate(1000, runif(16, -1, 1))
 temp_df <- as.data.frame(temp)
 temp_df[1,] <- 0
-ggplot(temp_df)+
-  geom_line(aes(x = seq(1,16), y = V1))+
-  geom_line(aes(x = seq(1,16), y = V2))+
-  geom_line(aes(x = seq(1,16), y = V3))+
-  geom_line(aes(x = seq(1,16), y = V4))+
-  geom_line(aes(x = seq(1,16), y = V5))+
-  geom_line(aes(x = seq(1,16), y = V6))+
-  geom_line(aes(x = seq(1,16), y = V7))+
-  geom_line(aes(x = seq(1,16), y = V8))+
-  geom_line(aes(x = seq(1,16), y = V9))+
-  geom_line(aes(x = seq(1,16), y = V10))+
-  geom_line(aes(x = seq(1,16), y = V11))
+temp_df <- data.table::as.data.table(temp_df)
+temp_df[,"x1"] <- seq(1,16)
+temp_long <- data.table::melt(temp_df,id.vars = "x1")
+ggplot(data = temp_long,aes(x1,value,group=variable))+
+  geom_line()
+

e7ca49cb74839909e9cef4f2be826253fbb809cf
AimjGuytidy <aimjguy@gmail.com>
2023-11-21 05:07:12 +0200

    Tmis analysis

--- tmis_updated.R
+++ tmis_updated.R
@@ -13,6 +13,27 @@ setwd(paste0("C:/Users/HP/Box/",
              "31_TMIS_analysis"))
 
 
-# get data
+# get data ####
 
 tmis_df <- read_excel("02_raw_data/tmis_data_october_2023.xlsx")
+
+# Categorize ages in 5s ####
+
+tmis_filter <- tmis_df|>
+  select(employeeid,gender,civilStatus,year_birth,position,qualificationLevel,
+         role,schoolName:districtName)|>
+  mutate(age = as.integer(format(Sys.Date(), "%Y")) - as.integer(year_birth),
+         age_categ = if_else(age >= 70, "70 and above",
+                                if_else(age >= 65, "65-69",
+                                        if_else(age >= 60, "60-64",
+                                                if_else(age >= 55, "55-59",
+                                                        if_else(age >= 50, "50-54",
+                                                                if_else(age >= 45, "45-49",
+                                                                        if_else(age >= 40, "40-44",
+                                                                                if_else(age >= 35, "35-39",
+                                                                                        if_else(age >= 30, "30-34",
+                                                                                                if_else(age >= 24, "24-29",
+                                                                                                        "18-23")))))))))))
+# Cross reference age categories with gender ####
+
+tmis_age_gender <- 

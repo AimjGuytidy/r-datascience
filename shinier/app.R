c7e529c547bb5af2b49237df824141ac8e28715a
AimjGuytidy <aimjguy@gmail.com>
2024-02-07 10:03:38 +0200

    Adding behaviour to the app

--- shinier/app.R
+++ shinier/app.R
@@ -1,13 +1,42 @@
 # clear all available objects
 rm(list=ls())
 # install shiny package
-install.packages("shiny")
+#install.packages("shiny")
 
 library(shiny)
+library(tidyverse)
 
 # creating app directory and file
-ui <- fluidPage("Hello, world!")
-server <- function(input, output, session) {}
-shinyApp(ui, server)
+
+# ui <- fluidPage("Hello, world!")
+# server <- function(input, output, session) {}
+# shinyApp(ui, server)
 
 
+# Adding UI controls
+
+ui <- fluidPage(
+  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+  verbatimTextOutput("summary"),
+  tableOutput("table"),
+  plotOutput("ploty")
+)
+
+server <- function(input, output, session) {
+  output$summary <- renderPrint({
+    dataset <- get(input$dataset,"package:datasets")
+    summary(dataset)
+  })
+  output$table <- renderTable({
+    dataset <- get(input$dataset, "package:datasets")
+    head(dataset,6)
+    
+  })
+  output$ploty <- renderPlot({
+    dataset <- get(input$dataset,"package:datasets")
+    nums <- Filter(is.numeric, dataset)
+    columns <- colnames(nums)
+    hist(nums[,1],col = "blue",border = "white", main = columns[1])
+  })
+}
+shinyApp(ui,server)

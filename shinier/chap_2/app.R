09a42277825f9dc7bf8afaaa2357014761f8a4f8
AimjGuytidy <aimjguy@gmail.com>
2024-02-09 09:50:45 +0200

    User Interface elements

--- shinier/chap_2/app.R
+++ shinier/chap_2/app.R
@@ -1,5 +1,7 @@
 library(shiny)
 
+animals <- c("dog", "cat", "mouse", "bird", "other", "I hate animals")
+
 # free text
 ui <- fluidPage(
   sliderInput("min", "Limit (minimum)", value = 20, min = 10, max = 50),
@@ -9,7 +11,29 @@ ui <- fluidPage(
   passwordInput("passcode","what's your password?"),
   textAreaInput("story", "Tell me about yourself", rows = 3),
   dateInput("date","Date of Birth"),
-  dateRangeInput("date_range","when do you want to go on vacation next?")
+  dateRangeInput("date_range","when do you want to go on vacation next?"),
+  selectInput("states","What's your favorite state?",state.name,multiple = TRUE),
+  radioButtons("pet", "What's your favorite animal?",animals),
+  radioButtons("rb", "choose One:",
+               choiceNames = list(
+                 icon("angry"),
+                 icon("smile"),
+                 icon("sad-tear")
+               ),
+               choiceValues = list("angry","happy","sad")),
+  checkboxGroupInput("checkit","What animals do you like?",animals),
+  checkboxInput("checker", "cleanup", value = TRUE),
+  checkboxInput("shutdown", "Shutdown"),
+  fileInput("upload", NULL),
+  actionButton("click","Click me!"),
+  actionButton("drink", "Drink me!", icon = icon("cocktail")),
+  fluidRow(
+    actionButton("click1","Click me!",class = "btn-danger"),
+    actionButton("drink", "Drink me!", class = "btn-lg btn-success")
+  ),
+  fluidRow(
+    actionButton("eat","Eat me!",class = "btn-block")
+  )
 )
 
 

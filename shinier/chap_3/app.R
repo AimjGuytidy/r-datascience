a53ece85075fb91331f30f6bcf012704bc3fa5c8
AimjGuytidy <aimjguy@gmail.com>
2024-02-13 11:01:21 +0200

    introduction to reactive expressions

--- shinier/chap_3/app.R
+++ shinier/chap_3/app.R
@@ -7,19 +7,26 @@ ui <- fluidPage(
   textOutput("text")),
   fluidRow(titlePanel("    ")),
   fluidRow(textInput("name", "What's your name?",placeholder = "Enter your name here!"),
-  textOutput("named"))
+  textOutput("named")),
+  fluidRow(titlePanel("\nExercises\n")),
+  fluidRow(
+    textInput("name1", "What's your name?"),
+    textOutput("greeting")
+  )
   
 )
 
 server <- function(input, output,session) {
   # back end logic
   texty <- reactive({
+    # reactive expression takes input and produce an output!
     paste0("Hello ", input$name, "!")
   })
   output$text <- renderText(paste("given your choice of a number, there are", input$count, "cows in the area"))
   output$named <- renderText({
     texty()
   })
+  output$greeting <- renderText(paste0("Hello ", input$name1))
   }
 
 # Run the application 

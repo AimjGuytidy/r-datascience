d9d8871ca6eeb4b7208453e6729b2cc4a0681e1e
AimjGuytidy <aimjguy@gmail.com>
2024-02-15 10:35:30 +0200

    creating our shiny app based on neiss injuries datasets

--- shinier/chap_4/app.R
+++ shinier/chap_4/app.R
@@ -5,70 +5,53 @@ library(vroom)
 library(shiny)
 
 # Getting data on the computer
-dir.create("shinier/datasets/neiss")
-download_path <- "shinier/datasets/neiss/"
-
-#Creating a function to download datasets 
-download <- function(name) {
-  url <- "https://github.com/hadley/mastering-shiny/tree/main/neiss/"
-  download.file(url = paste0(url,name),destfile = paste0(download_path,name),
-                quiet = TRUE)
-}
-
-#Downloading data
-download("injuries.tsv.gz")
-download("population.tsv")
-download("products.tsv")
+download_path <- "C:/Users/HP/source/repos/r-datascience/shinier/datasets/neiss/"
 
 #Importing the main dataset that we  will use: injuries
 injuries <- vroom::vroom(paste0(download_path,"injuries1.tsv"))
 products <- vroom::vroom(paste0(download_path,"products1.tsv"))
 population <- vroom::vroom(paste0(download_path,"population1.tsv"))
 
-#Exploration 
+prod_codes <- setNames(products$prod_code, products$title)
 
-selected <- injuries |>
-  filter(prod_code == 649)
-nrow(selected)
-selected |> 
-  count(location, wt = weight, sort = T)
-selected |>
-  count(body_part, wt = weight, sort = T)
-selected |>
-  count(diag, wt = weight, sort = T)
-
-summary_data <- selected |>
-  count(age, sex, wt = weight)
-summary_data
-
-ggplot(summary_data,aes(age,n,color = sex)) +
-  geom_line(size = 0.8) +
-  ylab("Estimated number of injuries") +
-  xlab("age")
-
-summary_population <- selected |>
-  count(age, sex, wt = weight) |>
-  left_join(population, by = c("age","sex")) |>
-  mutate(rate = n/population * 1e4)
-summary_population
-
-
-ggplot(summary_population,aes(age,rate,color = sex)) +
-  geom_line(na.rm = T,size = 0.8) +
-  ylab("Injuries rate per 10000") +
-  xlab("age")
-
-selected |>
-  sample_n(10) |>
-  pull(narrative)
+count_top <- function(df, var, n= 5) {
+  df |>
+    mutate({{var}} := fct_lump(fct_infreq({{var}}),n= n)) |>
+    group_by({{var}}) |>
+    summarize(n = as.integer(sum(weight)))
+}
 
 ui <- fluidPage(
-  
+  fluidRow(column(6,
+                  selectInput("code","Product",choices = prod_codes))),
+  fluidRow(column(4,dataTableOutput("diag")),
+           column(4,dataTableOutput("body_part")),
+           column(4,dataTableOutput("location"))),
+  fluidRow(plotOutput("age_sex"))
 )
 
 
 server <- function(input, output) {
-
+  selected <- reactive({injuries |>
+    filter(prod_code == input$code)})
+  summary_population <- reactive({selected() |>
+    count(age, sex, wt = weight) |>
+    left_join(population, by = c("age","sex")) |>
+    mutate(rate = n/population * 1e4)})
+  output$diag <- renderDataTable({
+    count_top(selected(), diag)}, options = list(dom = "t",searching = F)
+    )
+  output$body_part <- renderDataTable({
+    count_top(selected(), body_part)}, options = list(dom = "t",searching = F)
+    )
+  output$location <- renderDataTable({
+    count_top(selected(), location)}, options = list(dom = "t",searching = F)
+    )
+  output$age_sex <- renderPlot(
+    {ggplot(summary_population(), aes(age, rate, color = sex)) +
+      geom_line(na.rm = T, size = 0.8) +
+      labs(y = "Injuries rate per 10,000 People")}, res = 96
+  )
   
 }
 
